7c3a7fc8f7f68fe3137ac2e8f5e5d5c087487c32
Ilion Beyst <ilion.beyst@gmail.com>
2016-05-23 14:23:01 +0200

    essential packages

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,40 @@
-(require 'package)
-
+;; Space littering is bad
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+
+;; Pls no
+(setq inhibit-startup-screen t)
+
+(require 'package)
+
+;; Packages
+
+(setq package-user-dir "~/.emacs.d/elpa/")
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+
+(defun ensure-installed (&rest packages)
+  (mapcar
+   (lambda (package)
+     (unless (package-installed-p package)
+       (package-install package)))
+   packages))
+
+(ensure-installed 'helm
+		  'projectile
+		  'helm-projectile
+		  'magit
+		  'company
+		  'flycheck )
+
+;; helm
+(global-set-key (kbd "M-x") 'helm-M-x)
+(helm-mode 1)
+
+;; projectile
+(projectile-global-mode)
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
